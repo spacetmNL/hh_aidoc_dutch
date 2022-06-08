a9e48562bdb916ad7e50f3b3713b7a833740b137
spacetmNL <107124083+spacetmNL@users.noreply.github.com>
2022-06-08 15:22:59 +0200

    just a translation to dutch

--- hh_aidoc[ESX]/config.lua
+++ hh_aidoc[ESX]/config.lua
@@ -3,7 +3,6 @@
 -------------------
 Config = {}
 
-Config.Doctor = 0 -- Minimum Amount of EMS to work
-Config.Price = 2000
-Config.ReviveTime = 20000  --in msec
-
+Config.Doctor = 0 -- Minimaal aantal ambulance online
+Config.Price = 500
+Config.ReviveTime = 2000  --in msec
